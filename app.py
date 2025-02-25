2e69903e6ec14ecf35d7d9f3a76512839db8952d
Theerttha <theerttha.s@gmail.com>
2025-02-25 23:40:50 +0530

    a_lot

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 from flask import Flask, render_template, url_for, request, redirect,session
+
+from flask_session import Session
+
 app = Flask(__name__)
+app.config['SECRET KEY']="IEEE"
+
+app.config['SESSION_TYPE'] = 'filesystem'
+sess = Session()
 def request_func(nav):
+    
     if(nav=="1"):
         return redirect(url_for('index'))
     elif nav=="2":
         return redirect(url_for('list_team'))
+    elif nav=="3":
+        session['year']=[]
+        return redirect(url_for('events_list'))
+    elif nav=="4":
+        return redirect(url_for('about'))
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html')
+        notifications=[["Notification 1,content"],["Notification 2,content"],["Notification 3,content"]]
+        img_notification=["img_notification.jpg","img_notification.jpg","img_notification.jpg"]
+        return render_template('index.html',notifications=notifications[:-1],img_notification=img_notification[-1])
     if request.method == 'POST':
-        nav=request.form['nav']
+        nav=request.form.get("nav")
         return request_func(nav)
         
 @app.route('/members', methods=['POST', 'GET'])
@@ -36,7 +51,46 @@ def list_team():
             member_data=[["Irin Maria","Chairperson","3rd Year","ECE","Field of interest","linkedin link","img_link.webp"],["K Srihari","Secretary","2nd Year","EEE","Field of interest","linkedin link","img_link.webp"]]
             return render_template('members.html',member=member_data[ind-1],img=member_data[ind-1][-1])
         
-            
+@app.route('/events_list', methods=['POST', 'GET'])      
+def events_list():
+    d={2025:["Event 1","Event 2"],2024:["Event 1"],2023:[], 2022:[]}
+    if request.method=="GET":
+        if 'year' not in session:
+            session['year']=[]
+        
+        return render_template("events_list.html",year=session['year'],data=d) 
+    if request.method=="POST":
+        nav=request.form.get('nav')
+        if nav is not None:
+            return request_func(nav)
+        events_list=request.form.get('events_list')
+        if events_list is not None:
+            events_list=int(events_list)
+            year=session['year']
+            if events_list not in year:
+                year.append((events_list))
+            else:
+                year.remove((events_list))
+            session['year']=year
+            return render_template("events_list.html",year=session['year'],data=d)
+        event_data=request.form.get('event_data')
+        if event_data is not None:
+            html=""
+            for i in event_data:
+                if i==",":
+                    html+="_"
+                else:
+                    html+=i
+            html+=".html"
+            return render_template(html)
+@app.route('/about', methods=['POST', 'GET'])
+def about():
+    if request.method == 'GET':
+        return render_template('about.html')
+    if request.method == 'POST':
+        nav=request.form.get('nav')
+        return request_func(nav)
 if __name__ == "__main__":
+    sess.init_app(app)
     app.run(debug=True)
 
